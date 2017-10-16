0dd2ded72bf980b64a71d9681aec684d3ba03f47
Petr <xsicp01@gjk.cz>
2017-10-16 23:48:15 +0200

    Upload

--- server.js
+++ server.js
@@ -566,7 +566,7 @@ function onMovePlayer(data) {
 
 
 function onMapEdit(data) {
-	util.log(data);
+	util.log(data);//t
 	if(parseInt(data.block) == -1) {
 		var dropped = drop(items[map[parseInt(data.x)][parseInt(data.y)]].drop[0], items[map[parseInt(data.x)][parseInt(data.y)]].drop[1], items[map[parseInt(data.x)][parseInt(data.y)]].drop[2], items[map[parseInt(data.x)][parseInt(data.y)]].drop[3], items[map[parseInt(data.x)][parseInt(data.y)]].drop[4], playerById(this.id).inventory.hotbar[parseInt(data.active)].item)
 		var positions = giveItemToBestInventoryPosition(dropped.item, dropped.count, this.id);

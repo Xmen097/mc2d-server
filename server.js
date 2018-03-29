5249d845a77f530476d6cd04b3eb771607bd3548
Petr <xsicp01@gjk.cz>
2018-03-29 16:24:32 +0200

    Upload

--- server.js
+++ server.js
@@ -970,7 +970,7 @@ function onNewMessage(data) {
 								} else {
 									var itemParts = a.name.split('_');
 									for(var b of itemParts) {
-										var distance = levenshtein.get(args[1].toLowerCase(), b.toLowerCase()) + itemParts.length > 1 ? itemParts.length:0;
+										var distance = levenshtein.get(args[1].toLowerCase(), b.toLowerCase()) + (itemParts.length-1);
 										if(distance < smallestDistance) {
 											smallestDistance = distance;
 											possibleItem = a.name;

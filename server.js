d9ad864f45aaff5f3f19b97a53f712556113b099
Petr <xsicp01@gjk.cz>
2018-03-31 20:45:44 +0200

    Upload

--- server.js
+++ server.js
@@ -689,8 +689,6 @@ function onNewPlayer(data) {
 		if(err) {
 			util.log("Login server offline")
 		}
-		if(httpResponse) 
-			util.log(httpResponse);
 		if(body == "true" && !playerByName(validateString(data.name))) {
 			pg.connect(process.env.DATABASE_URL,function(err,pgClient,done) {
        	 		if(err){

1bf3f4c30bd708c9ddb20a7780cfb364d6f5f2e9
Petr <xsicp01@gjk.cz>
2018-04-01 15:39:19 +0200

    Upload

--- server.js
+++ server.js
@@ -1305,6 +1305,7 @@ function furnaceSmelting() {
 						if(furnaces[a].content[0].count == 0)
 							furnaces[a].content[0].item=undefined;
 						furnaces[a].content[2].item=c[1];
+						util.log(furnaces[a].content)
 					}
 				}
 				if(furnaces[a].content[0].item == c[0] && furnaces[a].content[1].item != undefined && items[furnaces[a].content[1].item].smelting != undefined && furnaces[a].fuelProgress == 0) {
